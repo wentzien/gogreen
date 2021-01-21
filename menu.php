b17a6dbe1c15925aee680a69d59fc98342294317
root <root@ip-10-0-0-26.ec2.internal>
2021-01-21 19:29:43 +0000

    start

---
+++ menu.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-default" role="navigation">
+
+<div class="navbar-header">
+  <a class="navbar-brand" href="/"><img width="75" src="https://s3.amazonaws.com/awstechbootcamp/GettingStarted/logo.png" /></a>
+</div>
+
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+  <ul class="nav navbar-nav">
+    <li>
+      <a href="load.php">Load Test</a>
+    </li>
+    <li>
+      <a href="rds.php">RDS</a>
+    </li>
+    <li>
+      <a href="upload.php"> Upload </a>
+    </li>
+  </ul>
+</div>
+
+</nav>
